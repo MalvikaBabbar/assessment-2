b17c99c8b49344cdbd3c47e219ca503e32769766
MalvikaBabbar <malvikababbar.91@gmail.com>
2022-08-12 19:12:57 +0530

    API data fetched

---
+++ main.js
@@ -0,0 +1,33 @@
+document.querySelector("#search").addEventListener('click', (e) => {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest()
+    const input=document.querySelector("#input-box").value;
+
+
+    const url = `https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-11`
+
+    xhr.open('GET', url)
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const values = JSON.parse(xhr.responseText)
+            console.log(values);
+            var output = ''
+            for (let i = 0; i < values.length; i++) {
+                output += `
+                
+                
+                <img id="img-box" src=${values[input-1].url} />
+                
+            `
+            }
+
+
+            document.querySelector('#api-cont').innerHTML = output
+        }
+    }
+
+
+    xhr.send()
+}
+)
