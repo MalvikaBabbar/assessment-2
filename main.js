e78b952673ba58e15ea18b215f415c637410dc80
MalvikaBabbar <malvikababbar.91@gmail.com>
2022-08-14 01:48:19 +0530

    Media Queries Added

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0)
-})
+
+//Swiper JS
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        560:{
+            slidesPerView:2,
+            spaceBetween:40
+        },
+        1023:{
+            slidesPerView:3,
+            spaceBetween:60
+        }
+    }
+  });
+
 
 /* =======Fetch API using AJAX(XHR)======*/
 document.querySelector("#search").addEventListener('click', (e) => {
